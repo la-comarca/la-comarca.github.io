7b553040f863f6abdbe03a5b2ecb99400b05b9d9
Juan Alvaro Diaz Raimond Kedilhac <62560061+jadrk040507@users.noreply.github.com>
2026-09-16 21:24:20 -0600

    Route group hub requests before base Worker

---
+++ api/worker-entry.mjs
@@ -0,0 +1,12 @@
+import baseWorker from './worker.mjs';
+import {catechismHub} from './catechism-hub.mjs';
+import {catechismHubExtra} from './catechism-hub-extra.mjs';
+
+export default {
+ async fetch(request,env,ctx){
+  const path=new URL(request.url).pathname.replace(/\/$/,'');
+  if(path==='/backoffice/hub/access'||/^\/backoffice\/hub\/access\/[a-f0-9-]{36}\/permissions$/i.test(path)||/^\/backoffice\/hub\/groups\/[a-f0-9-]{36}\/sessions$/i.test(path))return catechismHubExtra(request,env);
+  if(path.startsWith('/backoffice/hub/'))return catechismHub(request,env);
+  return baseWorker.fetch(request,env,ctx);
+ }
+};
